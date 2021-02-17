9c61b101d3252c8be85487af3eefeceeba490f26
joseph Martell <joseph.martell@gmail.com>
2021-02-16 23:20:36 -0800

    Commit Initial Test Code
    This code is more of a proof of concept. This code uses reflection to
    examine constructors, but does not convert arguments to strongly
    typed parameters for constructor variations.

---
+++ CLImber.Example/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using CLImber;
+
+namespace CLImber.Example
+{
+    class Program
+    {
+        static void CLImberConfig()
+        {
+            //register class as handler
+            //Should pass the command (as a string), the class that handles the command, and a set of translators)
+            _handler.RegisterCmdHandler<StatusCmdHandler>("status", new List<IArgumentTypeConverter>() { new ArgToInt() });
+        }
+
+        static CLIHandler _handler = new CLIHandler();
+        static void Main(string[] args)
+        {
+            CLImberConfig();
+            Console.WriteLine(_handler.Handle(new List<string>()));
+            Console.WriteLine(_handler.Handle(new List<string>() { "5" }));
+
+        }
+    }
+
+    public class StatusCmdHandler
+        : CommandBase
+    {
+        public StatusCmdHandler()
+        {
+            Console.WriteLine("Default constructor");
+        }
+
+        public StatusCmdHandler(string arg1)
+        {
+            Console.WriteLine("String constructor: " + arg1);
+        }
+
+        public StatusCmdHandler(int arg2)
+        {
+            Console.Write("Int constructor: " + arg2);
+        }
+    }
+
+    public class ArgToInt
+        : IArgumentTypeConverter
+    {
+        object IArgumentTypeConverter.ConvertArgument(string arg)
+        {
+            return int.Parse(arg);
+        }
+    }
+}
---
+++ CLImber/CLIHandler.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CLImber
+{
+    public class CLIHandler
+    {
+
+        private Type _handler;
+        private List<IArgumentTypeConverter> _converters;
+
+        public void RegisterCmdHandler<T>(string cmd, List<IArgumentTypeConverter> converters) where T : CommandBase
+        {
+            _handler = typeof(T);
+            _converters = converters;
+        }
+
+        public string Handle(IEnumerable<string> args)
+        {
+            var convertedParms = new List<object>();
+
+            for (int i = 0; i < args.Count(); i++)
+            {
+                convertedParms.Add(_converters[i].ConvertArgument(args.ElementAt(i)));
+            }
+            var ctors = _handler.GetConstructors().Where((info) => info.IsPublic);
+
+            var chosenCtor = from c in ctors
+                             where (c.GetParameters().Count() == args.Count())
+                                && (c.GetParameters().Select(p => p.ParameterType).Except(convertedParms.Select(a => a.GetType()))).Count() == 0
+                             select c;
+
+            if (chosenCtor.Count() > 1)
+            { 
+                return "Could not determine constructor based on passed parameters";
+            }
+
+            chosenCtor.First().Invoke(args.ToArray());
+            return "constructor called";
+        }
+
+        //protected IEnumerable<object> TranslateParameters(IEnumerable<string> args)
+        //{
+
+        //}
+    }
+
+    public class CommandBase
+    {
+
+    }
+
+    public interface IArgumentTypeConverter
+    {
+        object ConvertArgument(string arg);
+    }
+}
